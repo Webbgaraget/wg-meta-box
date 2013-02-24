13d38d05d9f8300e9128b5046892c98a1eb7a193
Erik Hedberg <erik@webbgaraget.se>
2013-02-24 10:07:38 +0100

    Adds plugin information

--- wg-meta-box.php
+++ wg-meta-box.php
@@ -1,2 +1,11 @@
 <?php
+/*
+	Plugin Name: WG Meta Box
+	Plugin URI: https://github.com/Webbgaraget/wg-meta-box/http://URI_Of_Page_Describing_Plugin_and_Updates
+	Description: A class facilitating programmatically creating meta boxes in WordPress.
+	Version: v0.5.2
+	Author: Webbgaraget
+	Author URI: http://www.webbgaraget.se/
+	License: MIT
+*/
 require_once( dirname( __FILE__ ) . '/classes/class-wg-meta-box.php');
