bd11b7821b0ae65a4f3876a70b77d9f9506d2a79
Simon Strandman <simon@webbgaraget.se>
2015-02-05 15:04:31 +0100

    JS to duplicate the section.

---
+++ assets/js/repeatable-group.js
@@ -0,0 +1,158 @@
+(function(win, doc, $)
+{
+	"use strict";
+
+	var RepeatableGroup = RepeatableGroup || function($groupContainer)
+	{
+		// The group container
+		this.$groupCointainer = $groupContainer;
+
+		// Add button
+		this.$button = this.$groupCointainer.find('.add-group-button');
+
+		// Current number of groups
+		this.currentNumberOfGroups = this.$groupCointainer.find('.group-repeatable-section').length;
+
+		// Name of group
+		this.groupName = this.$groupCointainer.data('label');
+
+		// Group label template
+		this.$groupTemplate = this.$groupCointainer.find('.group-repeatable-section').clone();
+
+		// Remove buttons
+		this.$removeButtons = this.$groupCointainer.find('.group-remove-button');
+
+		this.init();
+	};
+
+	RepeatableGroup.prototype = {
+		init : function()
+		{
+			// Init remove buttons
+			this.initRemoveButtons();
+			this.initListeners();
+		},
+
+		initListeners : function()
+		{
+			var self = this;
+			this.$button.on('click', function()
+			{
+				self.addGroup();
+			});
+		},
+
+		initRemoveButtons : function()
+		{
+			var self = this;
+
+			this.$removeButtons.off('click');
+
+			this.$removeButtons = this.$groupCointainer.find('.group-remove-button');
+
+			this.$removeButtons.on('click', function(evt)
+			{
+				var $this;
+				var groupNum;
+
+				$this = $(this);
+				groupNum = $this.data('num');
+				console.log("Removing " + groupNum);
+
+				evt.preventDefault();
+				self.removeGroup(groupNum);
+			});
+
+		},
+
+		addGroup : function()
+		{
+			var $group;
+
+			this.currentNumberOfGroups++;
+
+			$group = this.createGroup();
+
+			this.$button.before($group);
+
+			this.initRemoveButtons();
+		},
+
+		removeGroup : function(num)
+		{
+			// Remove group
+			$('#' + this.$groupCointainer.attr('name') + '-' + num).parent().remove();
+
+			this.initRemoveButtons();
+		},
+
+		createGroup : function()
+		{
+			var $group;
+			var $removeButton;
+			var num;
+			var groupName;
+
+			$group = this.$groupTemplate.clone();
+			groupName = $group.attr('name');
+			$removeButton = $group.find('.group-remove-button');
+
+			num = this.currentNumberOfGroups - 1;
+			console.log("Adding group " + num);
+			$removeButton.data('num', num);
+
+			$group.attr('id' , this.$groupCointainer.data('name') + '-' + num);
+
+			// Fix up attributes
+			$group.find('fieldset').each(
+				function(i, fieldset)
+				{
+					var $fieldset = $(fieldset);
+					var name = $fieldset.attr('name');
+					$fieldset.find('textarea, input, select, label').each(
+						function(j, element)
+						{
+							var $element = $(element);
+
+							// Set for attribute on labels, nothing more is needed
+							if ( $element.is('label') )
+							{
+								$element.attr('for', name + '-' + num );
+								return;
+							}
+
+							// Empty the new fields
+							if ( $element.is('textarea') )
+							{
+								$element.text('');
+							}
+							else
+							{
+								$element.attr('value', '');
+							}
+
+							// Set id's
+							$element.attr('id', name + '-' + num );
+
+							// Set name attributes
+							var oldName = $element.attr('name');
+							$element.attr('name', oldName.replace(/\[.*?\]/, "[" + num + "]" ));
+						}
+					);
+				}
+			);
+
+			return $group;
+		},
+	};
+
+	// Init for all groups
+	$(win).ready(function()
+	{
+		$('.group-repeatable').each(function(i, group)
+		{
+			var repeatable = new RepeatableGroup($(group));
+		});
+	});
+
+})(this, document, jQuery);
